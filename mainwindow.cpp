f14f18d57428e91c829c4eb0e0a7c599a5dcfe7f
Lukáš Tinkl <lukas@kde.org>
2014-05-16 15:21:49 +0200

    add copyright headers

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,21 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
-#include "placesmodel.h"
-
-#include "dialogs/persondialog.h"
-#include "dialogs/placedialog.h"
+/*
+ * Copyright (C) 2014 Lukáš Tinkl <lukas@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
 
 #include <QtSql>
 #include <QMessageBox>
@@ -11,6 +23,13 @@
 #include <QInputDialog>
 #include <QStandardPaths>
 #include <QDebug>
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "placesmodel.h"
+
+#include "dialogs/persondialog.h"
+#include "dialogs/placedialog.h"
 
 MainWindow::MainWindow(QWidget *parent):
     QMainWindow(parent),
