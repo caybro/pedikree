a30d55518c1e4b0d77d130d4170ea36a8dbeff9d
Lukáš Tinkl <lukas@kde.org>
2014-05-19 15:34:08 +0200

    complete the PersonDialog

--- dialogs/persondialog.cpp
+++ dialogs/persondialog.cpp
@@ -120,67 +120,58 @@ void PersonDialog::save()
     if (m_personID == -1) { // add person
         query.prepare("INSERT INTO People (first_name, surname, maiden_name, prefix, suffix, sex, "
                       "birth_date, birth_place, "
-                      "death_date, death_place, death_reason, burial_place "
-                      "contact_email, contact_phone, contact_web) "
+                      "death_date, death_place, death_reason, burial_place, "
+                      "contact_email, contact_phone, contact_web, "
+                      "nationality, religion, height, weight, "
+                      "color, eye_color, hair_color, occupation) "
                       "VALUES (:first_name, :surname, :maiden_name, :prefix, :suffix, :sex, "
                       ":birth_date, :birth_place, "
-                      ":death_date, :death_place, :death_reason, :burial_place "
-                      ":contact_email, :contact_phone, :contact_web)");
-        query.bindValue(":first_name", ui->leGivenNames->text());
-        query.bindValue(":surname", ui->leSurname->text());
-        query.bindValue(":maiden_name", ui->leMaidenName->text());
-        query.bindValue(":prefix", ui->lePrefix->text());
-        query.bindValue(":suffix", ui->leSuffix->text());
-        query.bindValue(":sex", ui->cbMale->isChecked() ? "M" : (ui->cbFemale->isChecked() ? "F" : ""));
-        query.bindValue(":birth_date", ui->leBirthDate->text());
-        query.bindValue(":birth_place", ui->leBirthPlace->currentText());
-        // TODO places id
-        if (ui->cbDeceased->isChecked()) {
-            query.bindValue(":death_date", ui->leDeathDate->text());
-            query.bindValue(":death_place", ui->leDeathPlace->currentText());
-            query.bindValue(":death_reason", ui->leDeathReason->text());
-            query.bindValue(":burial_place", ui->leBurialPlace->currentText());
-        }
-        query.bindValue(":contact_email", ui->email->text());
-        query.bindValue(":contact_phone", ui->phone->text());
-        query.bindValue(":contact_web", ui->web->text());
-
-        qDebug() << "Adding person with query" << query.lastQuery();
-
-        if (!query.exec()) {
-            qWarning() << Q_FUNC_INFO << "Query failed with" << query.lastError().text();
-        }
+                      ":death_date, :death_place, :death_reason, :burial_place, "
+                      ":contact_email, :contact_phone, :contact_web, "
+                      ":nationality, :religion, :height, :weight, "
+                      ":color, :eye_color, :hair_color, :occupation)");
     } else { // update person
         query.prepare("UPDATE People SET "
                       "first_name=:first_name, surname=:surname, maiden_name=:maiden_name, prefix=:prefix, suffix=:suffix, sex=:sex, "
                       "birth_date=:birth_date, birth_place=:birth_place, "
                       "death_date=:death_date, death_place=:death_place, death_reason=:death_reason, burial_place=:burial_place, "
-                      "contact_email=:contact_email, contact_phone=:contact_phone, contact_web=:contact_web "
+                      "contact_email=:contact_email, contact_phone=:contact_phone, contact_web=:contact_web, "
+                      "nationality=:nationality, religion=:religion, height=:height, weight=:weight, "
+                      "color=:color, eye_color=:eye_color, hair_color=:hair_color, occupation=:occupation "
                       "WHERE id=:id");
 
-        query.bindValue(":first_name", ui->leGivenNames->text());
-        query.bindValue(":surname", ui->leSurname->text());
-        query.bindValue(":maiden_name", ui->leMaidenName->text());
-        query.bindValue(":prefix", ui->lePrefix->text());
-        query.bindValue(":suffix", ui->leSuffix->text());
-        query.bindValue(":sex", ui->cbMale->isChecked() ? "M" : (ui->cbFemale->isChecked() ? "F" : ""));
-        query.bindValue(":birth_date", ui->leBirthDate->text());
-        query.bindValue(":birth_place", ui->leBirthPlace->currentText());
-        // TODO places id
-        if (ui->cbDeceased->isChecked()) {
-            query.bindValue(":death_date", ui->leDeathDate->text());
-            query.bindValue(":death_place", ui->leDeathPlace->currentText());
-            query.bindValue(":death_reason", ui->leDeathReason->text());
-            query.bindValue(":burial_place", ui->leBurialPlace->currentText());
-        }
-        query.bindValue(":contact_email", ui->email->text());
-        query.bindValue(":contact_phone", ui->phone->text());
-        query.bindValue(":contact_web", ui->web->text());
         query.bindValue(":id", m_personID);
+    }
 
-        if (!query.exec()) {
-            qWarning() << Q_FUNC_INFO << "Query failed with" << query.lastError().text();
-        }
+    query.bindValue(":first_name", ui->leGivenNames->text());
+    query.bindValue(":surname", ui->leSurname->text());
+    query.bindValue(":maiden_name", ui->leMaidenName->text());
+    query.bindValue(":prefix", ui->lePrefix->text());
+    query.bindValue(":suffix", ui->leSuffix->text());
+    query.bindValue(":sex", ui->cbMale->isChecked() ? "M" : (ui->cbFemale->isChecked() ? "F" : ""));
+    query.bindValue(":birth_date", ui->leBirthDate->text());
+    query.bindValue(":birth_place", ui->leBirthPlace->currentText());
+    // TODO places id
+    if (ui->cbDeceased->isChecked()) {
+        query.bindValue(":death_date", ui->leDeathDate->text());
+        query.bindValue(":death_place", ui->leDeathPlace->currentText());
+        query.bindValue(":death_reason", ui->leDeathReason->text());
+        query.bindValue(":burial_place", ui->leBurialPlace->currentText());
+    }
+    query.bindValue(":contact_email", ui->email->text());
+    query.bindValue(":contact_phone", ui->phone->text());
+    query.bindValue(":contact_web", ui->web->text());
+    query.bindValue(":nationality", ui->nationality->currentText());
+    query.bindValue(":religion", ui->religion->currentText());
+    query.bindValue(":height", ui->height->value());
+    query.bindValue(":weight", ui->weight->value());
+    query.bindValue(":color", ui->color->currentText());
+    query.bindValue(":eye_color", ui->eyeColor->currentText());
+    query.bindValue(":hair_color", ui->hairColor->currentText());
+    query.bindValue(":occupation", ui->occupation->text());
+
+    if (!query.exec()) {
+        qWarning() << Q_FUNC_INFO << "Query failed with" << query.lastError().text();
     }
 }
 
@@ -208,7 +199,10 @@ void PersonDialog::populateControls()
     query.prepare(QString("SELECT first_name, surname, maiden_name, prefix, suffix, sex, "
                           "birth_date, birth_place, "
                           "death_date, death_place, death_reason, burial_place, "
-                          "contact_email, contact_phone, contact_web "
+                          "contact_email, contact_phone, contact_web, "
+                          "nationality, religion, "
+                          "height, weight, "
+                          "color, eye_color, hair_color, occupation "
                           "FROM People "
                           "WHERE id=%1").arg(m_personID));
     if (!query.exec()) {
@@ -254,4 +248,17 @@ void PersonDialog::populateControls()
     ui->email->setText(query.value("contact_email").toString());
     ui->phone->setText(query.value("contact_phone").toString());
     ui->web->setText(query.value("contact_web").toString());
+
+    // personal
+    ui->nationality->setEditText(query.value("nationality").toString());
+    ui->religion->setEditText(query.value("religion").toString());
+
+    ui->height->setValue(query.value("height").toInt());
+    ui->weight->setValue(query.value("weight").toInt());
+
+    ui->color->setEditText(query.value("color").toString());
+    ui->eyeColor->setEditText(query.value("eye_color").toString());
+    ui->hairColor->setEditText(query.value("hair_color").toString());
+
+    ui->occupation->setText(query.value("occupation").toString());
 }
